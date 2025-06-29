bb6d408625416fd64e09bcef7c3ab5b5fc1b532d
j-mlo <joanna.r.davis@outlook.com>
2025-06-30 00:58:22 +0100

    Connect database, create superuser, prepare for deployment

--- meal_pal/settings.py
+++ meal_pal/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +30,8 @@ SECRET_KEY = 'django-insecure-94tt2468-)q=6p&8q0=by#kktvmd84l)%tmxfe^ru0ohq%(qf$
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    ,'.herokuapp.com'
+    '.herokuapp.com',
+    '127.0.0.1',
 ]
 
 
@@ -76,11 +81,15 @@ WSGI_APPLICATION = 'meal_pal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
