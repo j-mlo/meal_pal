bb2494d2fb6808d6d7f81bbc1bceed9de8faff53
j-mlo <joanna.r.davis@outlook.com>
2025-06-30 08:35:45 +0100

    Add custom Secret Key

--- meal_pal/settings.py
+++ meal_pal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-94tt2468-)q=6p&8q0=by#kktvmd84l)%tmxfe^ru0ohq%(qf$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
